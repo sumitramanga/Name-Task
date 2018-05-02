b661e04b0cc2d6a99b1f62d70501815ade7b623c
sumitra manga <845869602@bun2-206-12.local>
2018-05-03 09:35:28 +1200

    added iamge, js declaration and more text

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
+// Document ready prepares the dom, i.e loading js after the dom/html has loaded
 $(document).ready(function(){
-  $('#icon').click(function(){ //Click on icon
-    console.log('working');
 
-    //Clicking on icon will fade in and out the content within body
-    $('.body').fadeOut().fadeIn(function(){
+    icon();// Call the function in order for it to work.
+    text();
 
-      //body, name and icon will change colours on toggle
-      $('.body').toggleClass('backgroundColor');
-      $('.name').toggleClass('whiteColor');
-      $('.icon').toggleClass('whiteColor');
+  function icon () {
+
+    $('#icon').click(function(){ //Click on icon
+      console.log('working');//Check if the element is working within the console.log
+
+      //Clicking on icon will fade in and out the content within body
+      $('.body').fadeOut().fadeIn(function(){
+
+        //body, name and icon will change colours on toggle
+        $('.body').toggleClass('backgroundColor');
+        $('.name').toggleClass('whiteColor');
+        $('.icon').toggleClass('whiteColor');
+      });
+    });//click icon ends here
+
+  } //js function delcaration ends here
+
+  function text () {
+    $('.name').hover(function(){
+      $('.paragraph').text('Cat Calgue is a student at Yoobee School of Design studying Web and UX Design.');
     });
-  });//icon ends here
+  }
+
 }); //Document ready ends here
